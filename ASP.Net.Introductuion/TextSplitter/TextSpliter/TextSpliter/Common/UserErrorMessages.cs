a5dde75f5639a1b502e9fda55a55e65d355ec9d1
Dimitar Banchev <114139864+Dimitar0304@users.noreply.github.com>
2024-09-29 13:13:21 +0300

    ASP .NET Introduction

---
+++ ASP.Net.Introductuion/TextSplitter/TextSpliter/TextSpliter/Common/UserErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace TextSpliter.Common
+{
+    public static class UserErrorMessages
+    {
+        public const string Required = "The Text field is required";
+        public const string StringLenghtError =
+            "The field Text must be a string with a minimum length of {2} and maximum length of {1}";
+    }
+}
---
+++ ASP.Net.Introductuion/TextSplitter/TextSpliter/TextSpliter/Models/TextViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using TextSpliter.Common;
+
+namespace TextSpliter.Models
+{
+    /// <summary>
+    /// TextViewModel
+    /// </summary>
+    public class TextViewModel
+    {
+        [Required(ErrorMessage =UserErrorMessages.Required)]
+        [StringLength(30,MinimumLength =2,ErrorMessage =UserErrorMessages.StringLenghtError)]
+        public string Text { get; set; } = null!;
+        
+        public string SplitText { get; set; } = null!;
+    }
+}
