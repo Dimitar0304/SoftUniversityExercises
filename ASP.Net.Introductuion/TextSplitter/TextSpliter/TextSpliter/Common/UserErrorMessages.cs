4ac140be276579aa60b8886031f8c10ad23208d5
Dimitar Banchev <114139864+Dimitar0304@users.noreply.github.com>
2024-09-29 13:14:27 +0300

    Delete ASP.Net.Introductuion directory

--- ASP.Net.Introductuion/TextSplitter/TextSpliter/TextSpliter/Common/UserErrorMessages.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TextSpliter.Common
-{
-    public static class UserErrorMessages
-    {
-        public const string Required = "The Text field is required";
-        public const string StringLenghtError =
-            "The field Text must be a string with a minimum length of {2} and maximum length of {1}";
-    }
-}
--- ASP.Net.Introductuion/TextSplitter/TextSpliter/TextSpliter/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TextSpliter.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
